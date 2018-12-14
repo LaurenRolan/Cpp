8d11afa76dea82485ab026f52d2fd99949e2a7e7
LaurenRolan <lauren.rolan@ecole.ensicaen.fr>
2018-12-14 10:49:49 +0100

    TP7

---
+++ TP7/include/string_functions.h
@@ -0,0 +1,6 @@
+/* Author: Lauren Rolan
+ * File: string_functions.h
+ */
+
+ template <typename T>
+ bool 
---
+++ TP7/src/string_functions.cpp
@@ -0,0 +1,6 @@
+/* Author: Lauren Rolan
+ * File: string_functions.cpp
+ */
+
+ #include "string_functions.h"
+
